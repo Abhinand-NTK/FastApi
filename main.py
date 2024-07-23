0acdf558a851d8567a0d89828544aa2a67c8ebdc
AbhinandNtk <abhinandntkm@gmail.com>
2024-07-23 12:26:16 +0530

    Added Basic put post and get routes functions

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-ECHO is on.
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/",deprecated=True,description="This is the first route")
+async def root():
+    return {"message":"Hello World"}
+
+if __name__ == "__main__":
+   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+@app.post("/")
+def post():
+    return {"message":"Hello world I am ready to post something"}
+
+@app.put("/")
+def put():
+    return {"message":"Hello world I am ready to edit something"}
